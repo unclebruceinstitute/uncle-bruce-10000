17a1606e94db02cab73bad2a84123548968dce8b
UncleBruceHermes <unclebruceinstitute@gmail.com>
2026-06-06 15:07:15 +0800

    Fix: restore proper answers for English/Chinese, unify all 3 subjects with same template

--- v2/chinese/sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='s1cn-v2';
-const ASSETS=['./','./index.html','./questions.json','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.skipWaiting();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
