47382398b30a7c12c7fdfdeb2476fcc5c8d216e2
UncleBruceHermes <unclebruceinstitute@gmail.com>
2026-06-06 16:11:58 +0800

    v2: deploy unified MC apps - Math (green), English (blue), Chinese (red) with consistent format

---
+++ v2/math/sw.js
@@ -0,0 +1,5 @@
+const CACHE='s1math-v2';
+const ASSETS=['./','./index.html','./questions.json','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.skipWaiting();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
